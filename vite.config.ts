5e6e006a0231b7c7a13cd69ef99284ed503f21ae
Alepsios <ibednov@live.ru>
2023-01-05 23:00:31 +0300

    feat: init laravel 9 + vite + typescript + vue3 + vue-router + pinia

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin';
+
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(),
+    laravel({
+      input: ['resources/ts/main.ts'],
+      refresh: true,
+  }),],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
