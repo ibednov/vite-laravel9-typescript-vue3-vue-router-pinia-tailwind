2ed67c6d4b656f2c12a204cba1e26a8880ab8e3a
Alepsios <ibednov@live.ru>
2023-01-06 00:25:29 +0300

    fix: configs, delete unused, edit routes for SPA

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-    laravel({
-      input: ['resources/ts/main.ts'],
-      refresh: true,
-  }),],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    plugins: [
+        vue(),
+        laravel({
+            input: ["resources/ts/main.ts"],
+            refresh: true,
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./resources/ts/", import.meta.url)),
+        },
+    },
+});
